90fb1a9f5285d62027a85606aa22ade9aca92d3d
bennett <bennettoconnell13@gmail.com>
2026-03-15 22:19:56 -0400

    made code work on bennetts machine

--- web_based_llm/web_based_llm/settings.py
+++ web_based_llm/web_based_llm/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
